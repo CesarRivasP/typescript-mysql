3f120c9906a1366b1fedc7790ec0b36944f11eea
Franklin Hurtado <frankhurtado75@gmail.com>
2019-05-08 16:55:10 -0400

    IMplemented express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Codigo typescript');
+import Server from './server/server';
+
+const server = Server.init(3000);
+
+server.start(() => {
+  console.log('Servidor corriendo en el puerto 3000')
+});
+// el comando tsc compila todos los archivos de typscript en proyecto y genera la carpeta de distribucion
