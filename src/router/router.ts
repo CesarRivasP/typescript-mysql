cd4a0e0256fbcb3d515e10517d06112f5c528f33
Franklin Hurtado <frankhurtado75@gmail.com>
2019-05-09 22:35:08 -0400

    Implemented querys for the routes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,23 +1,53 @@
 import { Router, Request, Response } from 'express';
+import MySQL from '../mysql/mysql';
 
 
 
 const router = Router();
 
 router.get('/heroes', (request: Request , response: Response) => {
-  response.json({
-    ok: true,
-    message: 'Todo esta bien'
+
+  const query = `SELECT * FROM heroes`;
+
+  MySQL.executeQuery(query, (error: any, heroes: Object[]) => {
+    if(error){
+      response.status(400).json({
+        ok: false,
+        error
+      });
+    }
+    else {
+      response.json({
+        ok: true,
+        heroes
+      });
+    }
   })
 })
 
 router.get('/heroes/:id', (request: Request , response: Response) => {
   const id = request.params.id;
+  const escapeID = MySQL.instance.connection.escape(id)
 
-  response.json({
-    ok: true,
-    message: 'Todo esta bien',
-    id
+  const query = `
+    SELECT *
+      FROM heroes
+        WHERE id = ${escapeID}
+  `;
+
+  MySQL.executeQuery(query, (error: any, heroes: Object[]) => {
+    if(error){
+      response.status(400).json({
+        ok: false,
+        error
+      });
+    }
+    else {
+      response.json({
+        ok: true,
+        heroes: heroes[0]
+      });
+    }
   })
 })
 
