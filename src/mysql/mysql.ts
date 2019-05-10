e28189dcbda874bc2917b8e835cfb7b527784def
Franklin Hurtado <frankhurtado75@gmail.com>
2019-05-09 21:32:42 -0400

    Implemented geter method for instance

--- src/mysql/mysql.ts
+++ src/mysql/mysql.ts
@@ -23,6 +23,16 @@ export default class MySQL {
     this.connectingDB()
   }
 
+  // -- geters and seters --
+
+  // geter para obtener la instancia
+  public static get instance(){
+    // SI existe, regresa la instancia, sino, this._instance = new this(), que va a crear una nueva conexion.
+    // Si no existe, va a llamar al constructor para que cree la instancia, y eso se va a almacenar en la instancia
+    return this._instance || (this._instance = new this());
+    // Asi va establecer que cuando se llame muchas veces al get instance, siempre se utilice la misma instancia definida
+  }
+
   private connectingDB(){
     this.connection.connect((error: mysql.MysqlError) => {
       if(error){
